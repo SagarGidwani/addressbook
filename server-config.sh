592624116ead17efba83f5790a6e0880fd9de3b3
Sagar Gidwani <sagargidwani06@gmail.com>
2024-02-12 14:07:09 +0530

    mod

--- server-config.sh
+++ server-config.sh
@@ -1,6 +1,7 @@
 sudo yum install git -y 
-sudo yum install java-1.8.0-openjdk-devel -y  #java 11 not required here
-sudo yum install maven -y
+
+sudo yum install docker -y 
+sudo systemctl start docker 
 
 if [ -d "addressbook"]
 then
@@ -8,9 +9,9 @@ then
     cd /home/ec2-user/addressbook
     git pull origin b1
 else
-    git clone https://github.com/SagarGidwani/addressbook.git  #it will clone in the home dir of ec2-user because this job is run by slave 2 default user ec2-user
+    git clone https://github.com/SagarGidwani/addressbook.git 
     cd addressbook
-    git checkout b1  #by default it will go to master branch but the jenkins file is kept on b1 brancg and jenkins is alse confugured to pull from b1 branch
+    git checkout b1 
 fi
 
-mvn package
+sudo docker build -t $1:v$2 /home/ec2-user/addressbook
