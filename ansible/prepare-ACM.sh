59f6b3add563b0223f027f7690e502c568f0d00e
Sagar Gidwani <sagargidwani06@gmail.com>
2024-03-01 02:20:50 +0530

    ansible

--- ansible/prepare-ACM.sh
+++ ansible/prepare-ACM.sh
@@ -5,4 +5,4 @@ aws configure set aws_secret_access_key $2
 /home/ec2-user/.local/bin/ansible-inventory -i /home/ec2-user/ansible/inventory_aws_ec2.yml --graph
 chmod 400 /home/ec2-user/.ssh/id_rsa
 /home/ec2-user/.local/bin/ansible-playbook -i  /home/ec2-user/ansible/inventory_aws_ec2.yml /home/ec2-user/ansible/docker-deploy.yml -e "DOCKER_IMAGE=$3 DOCKER_REG_PASSWORD=$4"
-#rm /home/ec2-user/.ssh/id_rsa
+rm /home/ec2-user/.ssh/id_rsa
